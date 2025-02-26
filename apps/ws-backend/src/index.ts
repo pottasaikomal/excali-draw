d0415cf81e1b00277d18b8736390ef4015a6aee6
Sai Komal Potta <komal@Sais-MacBook-Air.local>
2025-02-26 16:25:20 +0530

    ws logic is written

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,25 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { decode } from "jsonwebtoken";
+import {JWT_TOKEN} from "./config";
 const wss = new WebSocketServer({port: 8080});
 
-wss.on("connection", (ws) => {
-    ws.on('error', console.error);
+wss.on("connection", (ws, request) => {
 
+    const url = request.url;
+    if(!url)
+    {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ?? "";
+
+    const decoded = jwt.verify(token, JWT_TOKEN);
+    if(!decoded || (typeof decoded === "object" && !decoded.userId))
+    {
+        ws.close();
+        return;
+    }
+    ws.on('error', console.error);
     ws.on('message', (data) => {
         ws.send("pong");
     })
